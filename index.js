8ac4bcd65efebd6a1be1b6fa42162b5fe79619a3
Mdashik-pradhan <mdashikpradhan75@gmail.com>
2021-11-15 20:00:22 +0600

    firebase service account added

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 const express = require('express')
 const app = express()
+const admin = require("firebase-admin");
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const ObjectId  = require('mongodb').ObjectId;
-const fileUpload = require('express-fileupload');
+// const fileUpload = require('express-fileupload');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('products'));
-app.use(fileUpload());
+// app.use(express.static('products'));
+// app.use(fileUpload());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hda5d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        } catch {
+
+        }
+
+    }
+    next();
+}
 
 
 async function run(){
@@ -26,12 +49,21 @@ async function run(){
         const productsCollection = database.collection("products");
         const ordersCollection = database.collection("orders");
         const reviewsCollection = database.collection("reviews");
+        const usersCollection = database.collection("users");
 
         // POST API
         app.post('/products', async (req, res) =>{
             const file = req.files;
             const product = req.body;
             const result = await productsCollection.insertOne(product);
+            res.json(result);
+        })
+
+        // USER
+        app.post('/users', async (req, res) =>{
+            const file = req.files;
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
             res.json(result);
         })
 
@@ -79,6 +111,26 @@ async function run(){
             const reviews = await cursor.toArray();
             res.json(reviews);
         });
+
+
+        // PUT 
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(403).json({ message: 'you do not have access to make admin' })
+            }
+
+        })
 
         // DELETE API
         app.delete('/products/:id', async (req, res) => {
